e79974ac1c50899d9d0ad786ad2abe19b7bcab1f
David <www.wealthydavid@gmail.com>
2024-02-25 16:29:25 +0100

    Update User.java

--- User.java
+++ User.java
@@ -5,14 +5,4 @@ import java.util.List;
 
 public class User {
 
-    private final List<Diary> mySecretDiary = new ArrayList<>();
-
-    public void createDiary(String username, String password) {
-        Diary mySoMuchLovedDiary = new Diary(username, password);
-        mySecretDiary.add(mySecretDiary);
-    }
-
-    public void queryDiaries() {
-        System.out.println("My secret diaries: " + mySecretDiary);
-    }
-}
+	private Diary secretDiary;
