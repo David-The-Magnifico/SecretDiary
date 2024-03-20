d3eb596e06d9b8fe427cea95d8c37193a83b986d
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-20 13:31:41 +0100

    Repos Design for Diary project

--- src/Data/Exceptions/InvalidUsernameException.java
+++ src/Data/Exceptions/InvalidUsernameException.java
@@ -1,7 +1,10 @@
 package Data.Exceptions;
 
 public class InvalidUsernameException extends RuntimeException {
+    private static final Object message = new Object();
+
     public InvalidUsernameException(String username) {
+        String message = new String(username);
         super(message);
     }
 }
