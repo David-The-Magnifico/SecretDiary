c860d1de4d1ef6acdca0b2d83ef57e28613558b3
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-20 13:31:13 +0100

    Repos Design for Diary project

---
+++ src/Services/DiaryServices.java
@@ -0,0 +1,18 @@
+package Services;
+
+import Data.Model.Diary;
+
+public class DiaryServices {
+    Diary createDiary(String username, String password) {
+        return null;
+    }
+
+    void deleteDiary(String username) {
+
+    }
+
+    long getDiarySize(){
+
+        return 0;
+    }
+}
