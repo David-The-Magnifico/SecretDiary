045f934b4960ca3f7d07cfde59c40fdb324542b1
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-24 23:57:39 +0100

    Repos Design for Diary project

--- src/Data/Repositories/EntryRepository.java
+++ src/Data/Repositories/EntryRepository.java
@@ -1,4 +1,34 @@
 package Data.Repositories;
 
-public class EntryRepository {
+import Data.Exceptions.UsernameAlreadyExistException;
+import Data.Model.Entry;
+
+import java.util.List;
+
+public interface EntryRepository {
+    Entry findById(int id);
+
+    Entry findEntriesByTitle(String title);
+
+    Entry findEntryByAuthor(String author);
+
+    boolean deleteByEntry(Entry entry);
+
+    Entry save(Entry entry) throws UsernameAlreadyExistException;
+
+    List<Entry> findAll();
+
+    Entry findByTitle(String title);
+
+    long count();
+
+    void deleteById(int id);
+
+    void delete(Entry entry);
+
+    void update(Entry entry);
+
+    Entry findByUsername(String username);
+
+    void deleteByUsername(String username);
 }
