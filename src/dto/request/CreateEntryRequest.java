ebae6554424c6885d7c52e81005d385d420013db
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-24 22:44:12 +0100

    Repos Design for Diary project

---
+++ src/dto/request/CreateEntryRequest.java
@@ -0,0 +1,13 @@
+package dto.request;
+
+public class CreateEntryRequest {
+    private String body;
+    private String title;
+    private String author;
+
+    public String getBody() {
+        return body;
+    }
+
+
+}
