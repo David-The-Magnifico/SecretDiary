4935bd019738bdca7a6c4107b653fab4a34d8503
OLADIPO DAVID <150271288+David-The-Magnifico@users.noreply.github.com>
2024-03-25 01:12:18 +0100

    DiaryController.java

--- src/Controllers/DiaryController.java
+++ src/Controllers/DiaryController.java
@@ -8,28 +8,5 @@ import java.util.Collection;
 public class DiaryController {
     private Diary diary;
 
-    public DiaryController(Diary diary) {
-        this.diary = diary;
-    }
-
-    public void addEntry(int id, String title, String body, String author) {
-        Entry newEntry = new Entry(id, title, body, author);
-        diary.addEntry(newEntry);
-    }
-
-    public Collection<? extends Entry> getEntries() {
-        return diary.getEntries();
-    }
-
-    public boolean isDiaryLocked() {
-        return diary.isLocked();
-    }
-
-    public void lockDiary() {
-        diary.setLocked(true);
-    }
-
-    public void unlockDiary() {
-        diary.setLocked(false);
-    }
+    
 }
